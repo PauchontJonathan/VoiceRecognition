9b9c17c4cec927e7f0c5fbb44cc5bc9dec3f42ec
PauchontJonathan <pauchontjonathan@gmail.com>
2020-12-06 23:00:35 +0100

    application done

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const app  = {
+
+  currentRecordingValue: '',
+  isRecording: false,
+  isRecordingFeedBack: '',
+
+  init: () => {
+
+    const verifySupport = 'speechSynthesis' in window;
+    console.log(verifySupport);
+
+    voiceContainer = document.querySelector('.box');
+    recordElement = document.querySelector('.voiceContainer-record');
+    outputElement = document.querySelector('.voiceContainer-output');
+
+    if (verifySupport) {
+      recordElement.addEventListener('click', app.launchRecognition);
+    }
+  },
+
+  launchRecognition: () => {
+    const recognition = new webkitSpeechRecognition() ||new SpeechRecognition() ;
+
+    recognition.start();
+
+    app.startRecognition(recognition);
+
+    app.resultRecognition(recognition);
+
+  },
+
+  startRecognition: (recognition) => {
+  recognition.onstart = () => {
+    app.isRecording = true;
+
+    const recordingDiv = document.createElement("div");
+    recordingDiv.classList.add('box-recording');
+    voiceContainer.appendChild(recordingDiv);
+
+    app.isRecordingFeedBack = 'Message audio en enregistrement';
+  };
+  },
+
+  resultRecognition: (recognition) => {
+    recognition.onresult = function(event) {
+      const transcript = event.results[0][0].transcript;
+      app.currentRecordingValue = transcript;
+      app.isRecording = false;
+      app.updateDisplay();
+    };
+  },
+
+  updateDisplay: () => {
+    if (!app.isRecording) {
+      recordingDiv = document.querySelector('.box-recording');
+      recordingDiv.remove();
+      outputElement.textContent = `"${app.currentRecordingValue}"`;
+    }
+  },
+
+};
+
+document.addEventListener('DOMContentLoaded', app.init);
